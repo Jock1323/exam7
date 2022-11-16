04b7db1bd8395ca63226889ee7e2e64f3a47ac4a
Jock1323 <mirsidiq2002@gmail.com>
2022-11-16 11:24:54 +0500

    change server port

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import categoriesRoute from "./routes/categories.route.js"
 import subcategoriesRoute from "./routes/subCategories.route.js"
 import loginRoute from "./routes/users.route.js"
 import products from "./routes/products.route.js"
+const PORT=process.env.PORT || 8080
 const app=express()
 app.use(express.json())
 app.use(categoriesRoute)
@@ -13,4 +14,4 @@ app.use(products)
 app.use(loginRoute)
 app.all("/*",(_,__,next)=>next(new customError(404,"Page not found")))
 app.use(errorHandlerMiddleware)
-app.listen(8080,console.log("hello from the server"))
+app.listen(PORT,console.log("hello from the server"))
